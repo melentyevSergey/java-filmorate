d0bc55bac96a9e4dc540a0bb6c9f7b3bcf03e959
melentyevSergey <melentyev.sergey@yandex.ru>
2023-04-25 23:54:37 +0300

    ТЗ_11

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO FILMORATE.PUBLIC."genre" ("name") VALUES ('Комедия');
+INSERT INTO FILMORATE.PUBLIC."genre" ("name") VALUES ('Драма');
+INSERT INTO FILMORATE.PUBLIC."genre" ("name") VALUES ('Мультфильм');
+INSERT INTO FILMORATE.PUBLIC."genre" ("name") VALUES ('Триллер');
+INSERT INTO FILMORATE.PUBLIC."genre" ("name") VALUES ('Документальный');
+INSERT INTO FILMORATE.PUBLIC."genre" ("name") VALUES ('Боевик');
+
+INSERT INTO FILMORATE.PUBLIC."mpa" ("RATING", "DESCRIPTION") VALUES ('G', 'у фильма нет возрастных ограничений');
+INSERT INTO FILMORATE.PUBLIC."mpa" ("RATING", "DESCRIPTION") VALUES ('PG', 'детям рекомендуется смотреть фильм с родителями');
+INSERT INTO FILMORATE.PUBLIC."mpa" ("RATING", "DESCRIPTION") VALUES ('PG-13', 'детям до 13 лет просмотр не желателен');
+INSERT INTO FILMORATE.PUBLIC."mpa" ("RATING", "DESCRIPTION") VALUES ('R', 'лицам до 17 лет просматривать фильм можно только в присутствии взрослого');
+INSERT INTO FILMORATE.PUBLIC."mpa" ("RATING", "DESCRIPTION") VALUES ('NC-17', 'лицам до 18 лет просмотр запрещён');
